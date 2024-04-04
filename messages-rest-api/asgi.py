a36e0c5580a942c1561f204f7cace5b6130a45a5
ofek <ofek.h@634.ai>
2024-04-04 11:05:33 +0300

    Renamed project directory

--- messages-rest-api/asgi.py
+++ messages-rest-api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for messeges_rest_api project.
+ASGI config for messages-rest-api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messeges_rest_api.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'messages-rest-api.settings')
 
 application = get_asgi_application()
--- messages-rest-api/urls.py
+++ messages-rest-api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for messeges_rest_api project.
+URL configuration for messages-rest-api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
